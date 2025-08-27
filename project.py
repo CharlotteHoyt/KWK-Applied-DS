5d5eb17e0f0497592460a34bf204b8d276ca942d
CharlotteHoyt <charlotte.hoyt@comcast.net>
2025-08-27 16:17:12 -0700

    Interacted with API to report the accession year for Claude Monet's Garden at Sainte-Adresse.

--- project.py
+++ project.py
@@ -7,5 +7,20 @@ import requests
 # fox = response.json()
 # print(fox['image'])
 
-response = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects")
+# response = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects")
+# print(response.status_code)
+# collection = response.json()
+# print(collection['objectID'])
+
+response = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects/437133")
 print(response.status_code)
+collection = response.json()
+# print(collection['title'])
+# print(collection['artistDisplayName'])
+# print(collection['acessionYear'])
+
+title = collection['title']
+artistDisplayName = collection['artistDisplayName']
+accessionYear = collection['accessionYear']
+
+print("The accession year for " + artistDisplayName + "'s \"" + title + "\" is " + accessionYear + ".")
