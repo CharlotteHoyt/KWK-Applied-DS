401a1cde8a731e51f201d0505b91cd161fb28668
CharlotteHoyt <charlotte.hoyt@comcast.net>
2025-08-26 17:27:15 -0700

    Made JSON object, got image key from dictionary, and printed its value.

--- project.py
+++ project.py
@@ -1,4 +1,8 @@
 import requests
 
 response = requests.get("http://randomfox.ca/floof")
-print(response.status_code)
+# print(response.status_code)
+# print(response.text)
+# print(response.json())
+fox = response.json()
+print(fox['image'])
