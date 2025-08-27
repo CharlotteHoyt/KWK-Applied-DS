851267a5b41a5a9a5f6bf2a23a0e85ccdf3e9037
CharlotteHoyt <charlotte.hoyt@comcast.net>
2025-08-27 15:34:55 -0700

    Created an API request with The Metropolitan Museum of Art Collection API.

--- project.py
+++ project.py
@@ -1,8 +1,11 @@
 import requests
 
-response = requests.get("http://randomfox.ca/floof")
+# response = requests.get("http://randomfox.ca/floof")
 # print(response.status_code)
 # print(response.text)
 # print(response.json())
-fox = response.json()
-print(fox['image'])
+# fox = response.json()
+# print(fox['image'])
+
+response = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects")
+print(response.status_code)
