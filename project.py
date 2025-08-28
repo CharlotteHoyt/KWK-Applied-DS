3a0b798ac71eb40aa84acc2eb2f86a94217f8b4e
CharlotteHoyt <charlotte.hoyt@comcast.net>
2025-08-28 14:42:38 -0700

    Used pandas to write a collection script, saved pandas DataFrame to a CSV file.

--- project.py
+++ project.py
@@ -48,6 +48,7 @@ response = requests.get(api_url)
 if response.status_code == 200:
     data = response.json()
     df = pd.DataFrame(data)
+    df.to_csv('met_collection_data.csv')
     print(df.head())
 else:
     print(f"Failed to fetch data: {response.status_code}")
