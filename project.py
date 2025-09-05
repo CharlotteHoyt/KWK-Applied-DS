56cdb65e830fdd759393fab64d30dc460259efa9
CharlotteHoyt <charlotte.hoyt@comcast.net>
2025-09-05 15:50:58 -0700

    Deleted collection script for filtered CMA dataset via API URL.

--- project.py
+++ project.py
@@ -2,20 +2,6 @@ import requests
 import pandas as pd
 import os
 
-# Collection script for filtered CMA dataset.
-""" api_url = "https://openaccess-api.clevelandart.org/api/artworks/?female_artists&created_after=1999&limit=1000"
-
-response = requests.get(api_url)
-
-if response.status_code == 200:
-    data = response.json()
-    # df = pd.DataFrame(data)
-    df = pd.json_normalize(data)
-    df.to_csv('cma_collection_data.csv')
-    print(df.head())
-else:
-    print(f"Failed to fetch data: {response.status_code}") """
-
 # Collection script for full CMA dataset.
 """ csv_url = "https://media.githubusercontent.com/media/ClevelandMuseumArt/openaccess/refs/heads/master/data.csv"
 
