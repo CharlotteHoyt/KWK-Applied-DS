f36a84fa8149eee6b3b7b474efbdd6ca47bdf174
CharlotteHoyt <charlotte.hoyt@comcast.net>
2025-09-05 17:52:23 -0700

    Manually cleaned all 68,000 lines of data - made 139 corrections. Also used replaced all to add quotes around arrays in column. Cleaned data is in a new file.

--- visualizations.py
+++ visualizations.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+df = pd.read_csv('cma_collection_data_dropped_columns.csv')
+
+target_array = df.loc[1, 'artist_tags']
+print(f'The arrray in the first row is: {target_array}')
+
+specific_element = target_array[1]
+print(f'The second element of that array is: {specific_element}')
