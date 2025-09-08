6181609bf6be8d2b32f83ab55dae29686c88f22e
CharlotteHoyt <charlotte.hoyt@comcast.net>
2025-09-08 15:37:03 -0700

    Used augmented assignment operator to make code more concise.

--- visualizations.py
+++ visualizations.py
@@ -32,25 +32,25 @@ while row < 68075:
 
     # Increase counts of any applicable categories. 
     if any("male" in item for item in separated_array):
-        male_artist_count = male_artist_count + 1
+        male_artist_count += 1
     if any("female" in item for item in separated_array):
-        female_artist_count = female_artist_count + 1
+        female_artist_count += 1
     if any("gender unknown" in item for item in separated_array):
-        gender_unknown_artist_count = gender_unknown_artist_count + 1
+        gender_unknown_artist_count += 1
     if any("Asian (from 1900 to present)" in item for item in separated_array):
-        asian_artist_count = asian_artist_count + 1
+        asian_artist_count += 1
     if any("Asian American" in item for item in separated_array):
-        asian_artist_count = asian_artist_count + 1
+        asian_artist_count += 1
     if any("Jewish" in item for item in separated_array):
-        jewish_artist_count = jewish_artist_count + 1
+        jewish_artist_count += 1
     if any("Latine and Hispanic Artists" in item for item in separated_array):
-        latine_and_hispanic_artist_count = latine_and_hispanic_artist_count + 1
+        latine_and_hispanic_artist_count += 1
     if any("LGBTQ+ (after 1900)" in item for item in separated_array):
-        lgbtq_artist_count = lgbtq_artist_count + 1
+        lgbtq_artist_count += 1
     if any("Black American Artists" in item for item in separated_array):
-        black_american_artist_count = black_american_artist_count + 1
+        black_american_artist_count += 1
 
-    row = row + 1
+    row += 1
 
 # Print artist_tags totals to the terminal.
 print(Fore.BLACK + "Male artist count: " + Fore.BLUE + f"{male_artist_count}")
